80032812d4445c19e7134f6e2ec086719a6950ed
Your Name <you@example.com>
2025-04-19 22:35:19 +0530

    auth added

--- spark.java
+++ spark.java
@@ -2,7 +2,13 @@ public class spark {
     public static void main(String[]args) {
         System.out.println("hello owrld");
     }
-    public void main(){
+    public void payment (){
         System.out.println("m1");
+    }
+    public void notifyuser(){
+        System.out.println("new file add");
+    }
+    public void auth(){
+        System.out.println("auth");
     }
 }
