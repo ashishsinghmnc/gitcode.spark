c461ab18a7d58c06b3ad76b845ed3f19374edbd1
Your Name <you@example.com>
2025-04-19 14:30:23 +0530

    file new added

--- spark.java
+++ spark.java
@@ -1,11 +1,8 @@
 public class spark {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         System.out.println("hello owrld");
     }
     public void main(){
         System.out.println("m1g");
-    }
-    public void notifyuser(){
-        System.out.println("hello users");
     }
 }
